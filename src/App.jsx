7d24be1ed85b2e79f616cf0ea1c01d41f363e7ac
manoxx <mbpi.obpl@gmail.com>
2024-03-03 22:03:42 +0530

    App.jsx is modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
-
-import './App.css'
+import NavBar from "./Components/Header/NavBar/NavBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import Footer from "./Components/Footer/Footer"
+import { SkeletonTheme } from "react-loading-skeleton"
+// import FoodGridModel from "./Components/FoodItems/FoodGridModel"
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className="font-job">
+
+      <SkeletonTheme baseColor= '#c9c9c9' highlightColor="#a3a3a3">
+        <BrowserRouter>
+          <NavBar />
+
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+            <Route path="/FoodGridModel/:id" element={<Home />} />
+
+          </Routes>
+
+          <Footer />
+
+        </BrowserRouter>
+      </SkeletonTheme>
+
+    </div>
   )
 }
 
